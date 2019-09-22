fd2c9fd3678ee0593063969519e236ba51a23c02
Greta Neverauskaite <kika0125@gmail.com>
2019-09-22 22:36:59 +0300

    made a main engine for menu started to layer the menu content

---
+++ js/inermenu.js
@@ -0,0 +1,15 @@
+//click > get the name of divs class, change it to the named html
+
+$(document).ready(function(){
+    $("main div").click(function () { /* only to chosen tags, this function will apply*/
+        let file = this.className;
+        //replace with html with modified divs name
+        file = file.replace('menu-', 'menu/') + ".html";
+        $("main").load(file);
+        console.log(file);
+    })
+});
+
+
+
+
